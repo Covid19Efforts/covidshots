fc873fa0f7b9025219e4758466163c7089d3ce23
sahils <sahils>
2021-06-19 17:51:30 +0530

    fix eslint errors

--- util.js
+++ util.js
@@ -1,23 +1,24 @@
 g_util_handle_idlejs = null;
 
 function PlayAudio() {
-    g_handle_audio_alarm.loop = true;
-    g_handle_audio_alarm.play();
+	g_handle_audio_alarm.loop = true;
+	g_handle_audio_alarm.play();
 }
 
 function PauseAudio() {
-    g_handle_audio_alarm.pause();
+	g_handle_audio_alarm.pause();
 }
 
 function UtilInit() {
-    /*sample - https://github.com/rchasman/Idle.js/blob/master/example/index.htm*/
-    g_util_handle_idlejs = new Idle({
-        //onHidden: onHiddenCallback,
-        //onVisible: onVisibleCallback,
-        onAway: OnUserIdle,
-        onAwayBack: OnIdleEnd,
-        awayTimeout: 5000 //away with 5 seconds of inactivity
-    }).start();
+	/*sample - https://github.com/rchasman/Idle.js/blob/master/example/index.htm*/
+	// eslint-disable-next-line no-undef
+	g_util_handle_idlejs = new Idle({
+		//onHidden: onHiddenCallback,
+		//onVisible: onVisibleCallback,
+		onAway: OnUserIdle,
+		onAwayBack: OnIdleEnd,
+		awayTimeout: 5000 //away with 5 seconds of inactivity
+	}).start();
 }
 
 function OnUserIdle()
@@ -27,5 +28,5 @@ function OnUserIdle()
 
 function OnIdleEnd()
 {
-    PauseAudio();//say user has left the code to auto refresh/book, and we start sounding alarm. Stop sound when user is back.
+	PauseAudio();//say user has left the code to auto refresh/book, and we start sounding alarm. Stop sound when user is back.
 }
